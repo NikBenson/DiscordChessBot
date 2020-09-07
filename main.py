fab6f0baf0d95a867e94c25c0a961fd9152027c7
Nik <NikBenson@pm.me>
2020-09-07 20:39:30 +0200

    implemented welcome message.

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from dotenv import load_dotenv
 import mysql.connector
 import requests
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="laravel",
+  password="awsxdrfvgz"
+)
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -13,7 +19,15 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print()
+    print('connected!')
 
+
+@client.event
+async def on_member_join(member):
+    if(True): #member.id):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f"Please link your lichess account in order to use this bot. https://my.link.com/?discord_id={member.id}"
+        )
 
 client.run(TOKEN)
